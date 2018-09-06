82566ef12397a28cfd37db92b8b81eb40144ccdc
yzx <nekomail@qq.com>
2018-09-06 08:33:40 +0800

    --练习完成

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
-import { Observable, of } from "rxjs";
+import { Observable, of, ObservableLike } from "rxjs";
 import { MessageService } from "./message.service";
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, map, tap } from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
@@ -31,6 +35,44 @@ export class HeroService {
       .pipe(
         tap(_ => this.log(`HeroService:fetched hero id=${id}`)),
         catchError(this.handleError<Hero>(`getHero id=${id}`))
+      );
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, httpOptions)
+      .pipe(
+        tap(_ => this.log(`update hero id=${hero.id}`)),
+        catchError(this.handleError<any>('updateHero'))
+      );
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, httpOptions)
+      .pipe(
+        tap(hero => this.log(`added hero w/ id=${hero.id}`)),
+        catchError(this.handleError<Hero>('addHero'))
+      );
+  }
+
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete<Hero>(url, httpOptions)
+      .pipe(
+        tap(hero => this.log(`deleted hero id=${id}`)),
+        catchError(this.handleError<Hero>('deleteHero'))
+      );
+  }
+
+  searchHeroes(keyword: string): Observable<Hero[]> {
+    if (!keyword.trim()) {
+      return of([]);
+    }
+    const url = `${this.heroesUrl}/?name=${keyword}`;
+    return this.http.get<Hero[]>(url)
+      .pipe(
+        tap(x => this.log(`found heroes matching ${keyword}`)),
+        catchError(this.handleError('searchHeroes', []))
       );
   }
 
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -19,6 +19,21 @@ export class HeroesComponent implements OnInit {
       .subscribe(heroes => this.heroes = heroes);
   }
 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) return;
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(x => x !== hero);
+    this.heroService.deleteHero(hero)
+      .subscribe();
+  }
+
   ngOnInit() {
     this.getHeroes();
   }
